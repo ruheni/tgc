40145fe10718216060264945fbc7041968fc2022
Kev Zettler <kevzettler@gmail.com>
2023-06-28 09:54:46 -0700

    bump build

--- prisma/zod/note.ts
+++ prisma/zod/note.ts
@@ -1,5 +1,5 @@
 import * as z from "zod"
-
+// NoteModel
 export const NoteModel = z.object({
   author: z.string().trim().min(1, { message: 'Is required.' }),
   body: z.string().trim().min(1, { message: 'Is required.' }),
